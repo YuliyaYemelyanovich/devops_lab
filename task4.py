e14e175a3ddabdf2767cb4714d833ba9ee4ff107
Yuliya Yemelyanovich <yuliya_yemelyanovich@epam.com>
2019-02-21 14:17:13 +0300

    Task 4 completed

---
+++ task4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+n = int(input())
+numbs = [int(i) for i in input().split()]
+sum = 0
+mult = 1
+for i in numbs:
+	if i > 0:
+		sum += i
+a = numbs.index(min(numbs))
+b = numbs.index(max(numbs))
+if (a > b):
+	a, b = b, a
+for i in numbs[a + 1:b]:
+	mult *= i
+print(sum, mult, end=' ')
