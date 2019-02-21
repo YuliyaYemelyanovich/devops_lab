27c4740dee68cb8b4bb68709914834fdf3297558
Yuliya Yemelyanovich <yuliya_yemelyanovich@epam.com>
2019-02-21 12:37:46 +0300

    Added sorted output in task 2

--- task2.py
+++ task2.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
 if __name__ == '__main__':
-	a = int(input("Enter A: "))
-	b = int(input("Enter B: "))
-	c = int(input("Enter C: "))
-	d = int(input("Enter D: "))
+	a,b,c,d = [int(i) for i in input().split()]
 	roots = set()
 	for i in range(-100, 101):
 		if(a*i**3 + b*i**2 + c*i +d == 0):
 			roots.add(i)
-	print(" ".join([str(i) for i in roots]))
+	l = list(roots)
+	l.sort()
+	print(" ".join([str(i) for i in l]))
