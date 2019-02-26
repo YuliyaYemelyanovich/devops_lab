52ef2cb22cfada0e1f8b946072770ec6eb0404f7
Yuliya Yemelyanovich <yuliya_yemelyanovich@epam.com>
2019-02-26 18:04:00 +0300

    implemented opened/closed stats and search by user login

---
+++ pr-stats.py
@@ -0,0 +1,26 @@
+import requests
+import getpass
+
+#def parse_args
+
+
+
+
+
+
+
+#getpass.getpass()
+uname = 'yuliyayemelyanovich'
+pwd = getpass.getpass()
+url = 'https://api.github.com/repos/alenaPy/devops_lab/pulls?page=1&per_page=100'
+all_pr = requests.get(url, auth=(uname, pwd), params={'state': 'all'}).json()
+closed_pr = requests.get(url, auth=(uname, pwd), params={'state': 'closed'}).json()
+open_pr = requests.get(url, auth=(uname, pwd), params={'state': 'open'}).json()
+
+print('All requests: %d \nOpened requests: %d \nClosed requests: %d' % (len(all_pr), len(closed_pr), len(open_pr)))
+
+
+for i in all_pr:
+	if i['user']['login'] == 'YuliyaYemelyanovich':
+		print(i['title'])
+
